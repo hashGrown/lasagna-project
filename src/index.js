c50ccfa13af4363be2692b6a941292f84fc16601
Alex J Choi <petitspark247@gmail.com>
2021-03-06 11:39:35 +0900

    Load image to webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Play from './play.svg';
 
 function component() {
     const element = document.createElement('div');
@@ -8,6 +9,12 @@ function component() {
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
   
+    // Add the image to our existing div.
+    const playIcon = new Image();
+    playIcon.src = Play;
+
+    element.appendChild(playIcon);
+
     return element;
   }
   
