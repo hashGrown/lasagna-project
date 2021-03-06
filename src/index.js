373067e62d0d82569865abc8725a121966de1415
Alex J Choi <petitspark247@gmail.com>
2021-03-06 17:57:10 +0900

    Display session info form section

--- src/index.js
+++ src/index.js
@@ -1,28 +1,70 @@
-// import _ from 'lodash';
 import './style.css';
-// import Play from './play.svg';
 
-// function component() {
-//     const element = document.createElement('div');
-//     const btn = document.createElement('button');
-  
-//       // Lodash, now imported by this script
-//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//     element.classList.add('hello');
-  
-//     // Add the image to our existing div.
-//     const playIcon = new Image();
-//     playIcon.src = Play;
+// 변수 선언 및 초기화
+const sessionInfo = {
+    goal: "",
+    pomodoroLength: 25,
+    shortBreakLength: 5,
+    longBreakLength: 15,
+    sessionNumber: 4,
+  };
 
-//     element.appendChild(playIcon);
+// 첫 페이지 로딩
+function displaySessionForm() {
+    const element = document.createElement('main');
+    element.classListName = 'main container';
 
-//     // Add button
-//     btn.innerHTML = 'Click me and check the console!';
-//     btn.onclick = printMe;
-  
-//     element.appendChild(btn);
+    const sessionForm = document.createRange().createContextualFragment(`
+    <section class="timer">
+        <form class="form col">
+          <div class="form__item col">
+            <label class="form__label">Goal</label>
+            <input class="form__input-num" type="number" />
+          </div>
+          <div class="form__item row">
+            <div class="col">
+              <label class="form__label">Pomodoro (min)</label>
+              <input class="form__input-num pomodoro-length" type="number" />
+            </div>
+            <div class="col">
+              <label class="form__label">Number of Cycle</label>
+              <input class="form__input-num session-number" type="number" />
+            </div>
+          </div>
+          <div class="form__item row">
+            <div class="col">
+              <label class="form__label">Short Break (min)</label>
+              <input class="form__input-num shortBr-length" type="number" />
+            </div>
+            <div class="col">
+              <label class="form__label">Long Break (min)</label>
+              <input class="form__input-num longBr-length" type="number" />
+            </div>
+          </div>
+          <input type="submit" class="form__submit-btn" value="quick start" />
+        </form>
+      </section>
+    `);
+    element.appendChild(sessionForm);
 
-//     return element;
-//   }
-  
-//   document.body.appendChild(component());
+    return element;
+}
+
+// 변수 업데이트
+function updateSessionInfo() {
+    sessionInfo.goal = document.querySelector(".pomodoro-length").value;
+    sessionInfo.goal = document.querySelector(".session-number").value;
+    sessionInfo.goal = document.querySelector(".shortBr-length").value;
+    sessionInfo.goal = document.querySelector(".longBr-length").value;
+}
+
+// 타이머 실행
+function runSession () {
+
+}
+
+// 타이머 로딩
+
+
+
+document.body.appendChild(displaySessionForm());
